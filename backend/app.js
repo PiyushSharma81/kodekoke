b013348d7291cfe0b0d3475d0f02095e7ca1e42a
PiyushSharma81 <143711290+PiyushSharma81@users.noreply.github.com>
2025-02-19 23:12:41 +0530

    first commit

--- backend/app.js
+++ backend/app.js
@@ -43,5 +43,8 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
-
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
 module.exports = app;
--- frontend/src/helper.js
+++ frontend/src/helper.js
@@ -1,1 +1,3 @@
 export const api_base_url = "http://localhost:3000";
+// If you want to deploy website online you have to put the online backend url here 
+// export const api_base_url = "https://online-backend-url";
