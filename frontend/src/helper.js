9d9ff7daf70d9c4674b9e54378e9b170dea04670
PiyushSharma81 <143711290+PiyushSharma81@users.noreply.github.com>
2025-02-28 17:58:15 +0530

    Update helper.js

--- frontend/src/helper.js
+++ frontend/src/helper.js
@@ -1,3 +1,3 @@
-export const api_base_url = "http://localhost:3000";
+// export const api_base_url = "http://localhost:3000";
 // If you want to deploy website online you have to put the online backend url here 
-// export const api_base_url = "https://online-backend-url";
+export const api_base_url = "http://codecock-next2.onrender.com";
